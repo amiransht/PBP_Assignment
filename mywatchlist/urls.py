e18453df5b76177d5911f679802ed567b603e43c
amiransht <amira19nisrina@gmail.com>
2022-09-20 16:26:14 +0700

    add path

--- mywatchlist/urls.py
+++ mywatchlist/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.urls import path, include
 from mywatchlist.views import show_html
 from mywatchlist.views import show_xml 
 from mywatchlist.views import show_json
@@ -6,6 +7,7 @@ from mywatchlist.views import show_json
 app_name = 'mywatchlist'
 
 urlpatterns = [
+    path('', include('mywatchlist.urls')),
     path('html/', show_html, name='show_html'),
     path('xml/', show_xml, name='show_xml'),
     path('json/', show_json, name='show_json'),
